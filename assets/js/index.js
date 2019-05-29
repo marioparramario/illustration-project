d09fbd51e477b1de539db71a6bbc63cd76b248c0
Mario Parra <hello@oqgraphic.com>
2019-05-29 15:33:53 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,77 @@
+// Order
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('cooking'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/cooking-data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('groceries'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/groceries-data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('processing'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/processing-data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('received'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/received-data.json'
+})
+
+// Delivery
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bike'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/bike-data.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('car'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/car-data.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('delivered'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/delivered-data.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('generic'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/generic-data.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('scooter'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/scooter-data.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('walking'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/assets/js/walking-data.json'
+})
